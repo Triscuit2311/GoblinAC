d0b2e130126a05d233bcf03e51016ec3196a48c4
dtrisciuzzi <dante.trisciuzzi@snhu.edu>
2022-04-15 08:10:27 -0400

    Client Handshake

--- Client/Handshake/Handshake.cs
+++ Client/Handshake/Handshake.cs
@@ -6,6 +6,37 @@ namespace Goblin.Client.Handshake
 {
     public class Handshake : BaseScript
     {
-       
+        private bool debugusehb = true;
+
+        [Command("togglehb")]
+        public void ToggleHeartbeat()
+        {
+            debugusehb = !debugusehb;
+        }
+        
+        [Command("sendbadhb")]
+        public void BadHeartbeat()
+        {
+            TriggerServerEvent("HeartbeatCB", "BADB0I");
+        }
+        
+        public Handshake()
+        {
+            EventHandlers["RequestClientHeartbeat"] += new Action<int>(DispatchHeartbeat);
+            
+        }
+        private void DispatchHeartbeat(int seed)
+        {
+            while (LocalPlayer == null || LocalPlayer.Character == null)
+            {
+                Wait(100);
+            }
+
+            if (!debugusehb) return;
+            
+            TriggerServerEvent("HeartbeatCB", 
+                (seed ^ (LocalPlayer.Character.NetworkId * 1000000)).ToString());
+        }
+
     }
 }
