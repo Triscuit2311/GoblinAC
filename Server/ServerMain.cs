ce66a3679c65f37d2da9bbcbfe5343b854140003
dtrisciuzzi <dante.trisciuzzi@snhu.edu>
2022-04-15 08:19:51 -0400

    Disabled resharper unused warnings

--- Server/ServerMain.cs
+++ Server/ServerMain.cs
@@ -1,6 +1,7 @@
-using System;
-using System.Threading.Tasks;
 using CitizenFX.Core;
+// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable StringLiteralTypo
 
 namespace Goblin.Server
 {
