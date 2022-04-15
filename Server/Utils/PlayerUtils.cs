4ad157ae9ee0191577aa33b38e25ad1d8feed62b
dtrisciuzzi <dante.trisciuzzi@snhu.edu>
2022-04-15 10:49:10 -0400

    Added PlayerUtils.cs

---
+++ Server/Utils/PlayerUtils.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Linq;
+using CitizenFX.Core;
+namespace Goblin.Server.Utils
+{
+    public static class PlayerUtils
+    {
+        public static bool PlayerDisconnected(string id, PlayerList players)
+        {
+            var playerDisconnected = true;
+            foreach (var player in players)
+            {
+                if (player.Identifiers["fivem"] != id) continue;
+                playerDisconnected = false;
+            }
+
+            return playerDisconnected;
+        }
+
+        public static Player GetPlayerByFiveMid(string id, PlayerList players)
+        {
+            return players.FirstOrDefault(player => player.Identifiers["fivem"] == id);
+        }
+    }
+}
