53c8e9543c2e27939eb54325d763861e8ba674f1
dtrisciuzzi <dante.trisciuzzi@snhu.edu>
2022-04-15 09:29:00 -0400

    Player Reporting framework

---
+++ Server/Reporting/PlayerReport.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using CitizenFX.Core;
+
+namespace Goblin.Server.Reporting
+{
+    public class PlayerReport
+    {
+        private string PlayerName;
+        private IdentifierCollection Ids;
+        public Dictionary<string, string> ReportStrings;
+
+        public PlayerReport(string playerName, IdentifierCollection ids, Dictionary<string, string> reportStrings)
+        {
+            PlayerName = playerName;
+            Ids = ids;
+            ReportStrings = reportStrings;
+        }
+    }
+}
---
+++ Server/Reporting/ReportManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using CitizenFX.Core;
+
+namespace Goblin.Server.Reporting
+{
+    public class ReportManager : BaseScript
+    {
+        private Dictionary<string, PlayerReport> _reports;
+
+        public ReportManager()
+        {
+            _reports = new Dictionary<string, PlayerReport>();
+            EventHandlers["AddOrModifyReportString"] += new Action<Player, string, string>(AddOrModifyReportString);
+        }
+
+        private void AddOrModifyReportString(Player player, string reportType, string report )
+        {
+            var id = player.Identifiers["fivem"];
+            if (!_reports.ContainsKey(id))
+            {
+                _reports.Add(id, new PlayerReport(
+                    player.Name,
+                    player.Identifiers,
+                    new Dictionary<string, string>()));
+            }
+            _reports[id].ReportStrings[reportType] = $"[{DateTime.Now}] {report}";
+        }
+        
+    }
+}
