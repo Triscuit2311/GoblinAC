dab21ed87bf19ebe92d0c5989c4e98aeeda3cef7
dtrisciuzzi <dante.trisciuzzi@snhu.edu>
2022-04-16 18:55:27 -0400

    Formatting

--- Client/ClientMain.cs
+++ Client/ClientMain.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using CitizenFX.Core;
+
 // ReSharper disable UnusedType.Global
 // ReSharper disable UnusedMember.Global
 // ReSharper disable StringLiteralTypo
@@ -22,9 +23,8 @@ namespace Goblin.Client
         [Tick]
         public Task OnTick()
         {
-            // DrawRect(0.5f, 0.5f, 0.5f, 0.5f, 255, 255, 255, 150);
-
             return Task.FromResult(0);
         }
     }
 }
+
