922b855806a0b822fd00a1fd5b49ddb18e9a9bb5
dtrisciuzzi <dante.trisciuzzi@snhu.edu>
2022-04-16 20:11:17 -0400

    Refactoring

---
+++ Server/Crypto/KeyUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Goblin.Server.Crypto;
+
+internal class KeyUtils
+{
+    internal static int[] GetNumericalKeys(int num)
+    {
+        var gen = new Random();
+        var keys = new List<int>();
+        for (int i = 0; i < num; i++)
+        {
+            keys.Add(gen.Next(-10000,10000));
+        }
+        return keys.ToArray();
+    }
+    
+    internal static string GetUnicodeString(int len)
+    {
+        var r = new Random();
+        var sb = new StringBuilder();
+        for (var i = 0; i < len; i++)
+        {
+            sb.Append(Unicode.UnicodeChars[r.Next(0,10000)]);
+        }
+        return sb.ToString();
+    }
+}
