577a3286d6606250afce09d3f9d07b74ebc1863d
Triscuit2311 <dante.trisciuzzi@snhu.edu>
2022-04-19 17:59:21 -0400

    Added Lua Webhook extensions

--- dist/Server/WebHooks.lua
+++ dist/Server/WebHooks.lua
@@ -1,6 +1,6 @@
 print("Started Discord webhooks")
 
-AddEventHandler("Goblin::Extensions::SendDiscordWebhook", function(webhookUrl, avatar, title, message, footer, color)
+AddEventHandler("Goblin::Extensions::SendDiscordEmbed", function(webhookUrl, avatar, title, message, footer, color)
 	local connect = {
 		{
 		  	["color"] = color,
@@ -12,4 +12,9 @@ AddEventHandler("Goblin::Extensions::SendDiscordWebhook", function(webhookUrl, a
 		  }
 	  }
 	PerformHttpRequest(webhookUrl, function(err, text, headers) end, 'POST', json.encode({username = "Goblin", embeds = connect, avatar_url = avatar}), { ['Content-Type'] = 'application/json' })
+end)
+
+AddEventHandler("Goblin::Extensions::SendDiscordMessage", function(webhookUrl, avatar, message)
+
+	PerformHttpRequest(webhookUrl, function(err, text, headers) end, 'POST', json.encode({username = "Goblin",  avatar_url = avatar, content = message}), { ['Content-Type'] = 'application/json' })
 end)
