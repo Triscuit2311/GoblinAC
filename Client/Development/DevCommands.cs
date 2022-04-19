9d90edccfe6767781436b346b9c86046d0df2d99
Triscuit2311 <dante.trisciuzzi@snhu.edu>
2022-04-19 11:42:44 -0400

    Added build vars + centralized commands

---
+++ Client/Development/DevCommands.cs
@@ -0,0 +1,46 @@
+﻿#if DEVELOPMENT_BUILD
+using System;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using Goblin.Shared;
+
+namespace Goblin.Client.Development;
+
+public class DevCommands : BaseScript
+{
+    [Command("debugprintreports")]
+    public void PrintReports()
+    {
+        TriggerServerEvent("PrintReports");
+    }
+        
+    [Command("testkeys")]
+    public void RunKeys()
+    {
+        TriggerServerEvent("RunKeyTests");
+    }
+    
+    [Command("tryev")]
+    public async Task TryEvent()
+    {
+        TriggerEvent("Goblin::Client::EventProxy", "funevent", "sssss");
+        Random genny = new Random();
+        for (var i = 0; i < 10; i++)
+        {
+            TriggerEvent("Goblin::Client::EventProxy", 
+                SharedUtils.RandomAscii(genny.Next(5,30)), 
+                "A",
+                "B",
+                genny.Next(-100,100), 
+                genny.NextDouble());
+            await Delay(100);
+        }
+    }
+    
+    [Command("trylua")]
+    private void tryLua()
+    {
+        TriggerEvent("luaexamples:reverse");
+    }
+}
+#endif
