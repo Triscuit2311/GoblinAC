7a743a93ee7f0b22df5ba60391fa5cd03455668f
Triscuit2311 <dante.trisciuzzi@snhu.edu>
2022-04-19 11:41:08 -0400

    Refactor + NeedsKey state

---
+++ Server/Crypto/PlayerKeyManager.cs
@@ -0,0 +1,22 @@
+﻿namespace Goblin.Server.Crypto;
+
+public class PlayerKeyManager
+{
+    public string ClientKey;
+    public int[] NumericalKeys;
+    public bool NeedsKeys;
+
+    public PlayerKeyManager(string clientKey, int[] numericalKeys)
+    {
+        this.NumericalKeys = numericalKeys;
+        this.ClientKey = clientKey;
+        NeedsKeys = true;
+    }
+
+    public PlayerKeyManager()
+    {
+        this.NumericalKeys = new int[]{};
+        this.ClientKey = "";
+        NeedsKeys = true;
+    }
+}
