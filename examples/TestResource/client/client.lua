f831f18091c4237c849d29b57d54f88d77f2439a
Triscuit2311 <dante.trisciuzzi@snhu.edu>
2022-10-15 17:30:28 -0400

    update lua example

--- examples/TestResource/client/client.lua
+++ examples/TestResource/client/client.lua
@@ -1,9 +1,19 @@
 
+-- Example of a proxy function
 function Proxy(...)
     TriggerEvent('Goblin::Client::EventProxy', ...)
 end
 
+
 RegisterCommand('luatest', function(source, args)
-    Proxy("FakeScript::Server::GiveMoney", 123, 9.29123, "Cash")
+    
+    -- Old server trigger
     --TriggerServerEvent("FakeScript::Server::GiveMoney", 123, 9.29123, "Cash")
+    -- Since we do not have a client event handler on the server side
+    -- TrigerServerEvent(...) will not do anything.
+
+    -- Proxy the through Goblin event instead
+    Proxy("FakeScript::Server::GiveMoney", 123, 9.29123, "Cash")
+
+    
 end, false)
--- examples/TestResource/server/server.lua
+++ examples/TestResource/server/server.lua
@@ -1,4 +1,10 @@
 
+-- Since we do remove the handler waiting for a client event,
+-- This cannot be triggered from the client side
+
+--RegisterServerEvent('FakeScript::Server::GiveMoney')
+
+-- Just prints out whatever we pass to it
 AddEventHandler("FakeScript::Server::GiveMoney", function(...)
     for i,v in ipairs({...}) do
         print(v)
